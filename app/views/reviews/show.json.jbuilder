1fa606e33284ee4354acb50e07e9844ecfba80d8
Martin Laws <martinblaws@gmail.com>
2015-11-16 19:23:34 +0000

    rotten mangoes initial commit - whoops

---
+++ app/views/reviews/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @review, :id, :user_id, :movie_id, :text, :rating_out_of_ten, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoes::Application.config.secret_key_base = '2d0a9fefd76b9fc25dcc44fac1586c284f31f73bc691281cbce90e3ee3c8c21e913f9935f814d17abf5c6987d07caf9d06210c4148606f58c88045802a5523c1'
